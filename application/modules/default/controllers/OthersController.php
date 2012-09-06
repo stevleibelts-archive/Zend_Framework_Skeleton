f154bb1efe894134e2df4232a9c764174a604f0d
stevleibelt <nvb@localhost.localdomain>
2012-09-06 22:44:08 +0200

    Initial commit

---
+++ application/modules/default/controllers/OthersController.php
@@ -0,0 +1,19 @@
+<?php
+
+class Default_OthersController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        echo 'default-others-index';
+        // action body
+    }
+
+
+}
+
---
+++ application/modules/module2/controllers/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+
+class Module2_IndexController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        echo 'module2-index-index';
+        // action body
+    }
+
+
+}
+
